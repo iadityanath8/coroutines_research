4d70f27f10944a6d210a26c3b38fa4af9dfd9949
iadityanath8 <iadityanath8@gmail.com>
2024-03-09 15:08:17 +0530

    explored sleep functionality in scheduler

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-## BASIC RESEARCH ON COROUTINES AND ASYNCHROUNOUS EXECUTION 
+
+# Experiment/Research: Round Robin Basic Scheduler Implementation
+
+## Introduction:
+This experiment demonstrates a basic implementation of a Round Robin scheduler using Python. A Round Robin scheduler is a preemptive CPU scheduling algorithm that allocates a fixed time slice to each process or task in a circular queue. Once a task's time slice expires, it is put back at the end of the queue, allowing other tasks to execute in the meantime.
+
+## Code Overview:
+- The code consists of a `Scheduler` class responsible for managing the execution of tasks.
+- Tasks are added to the scheduler using `wait` and `wait_until` methods.
+- `run` method executes tasks in a round-robin manner until the call queue and wait list are empty.
+- The experiment includes two sample functions `f1` and `f2` which represent tasks to be executed by the scheduler.
+
+## Usage:
+- To use the scheduler, create an instance of `Scheduler`.
+- Add tasks using `wait` or `wait_until` methods.
+- Start the scheduler by calling the `run` method.
+
+## Example:
+```python
+sched = Scheduler()
+
+def f1(x, stop):
+    if x < stop:
+        print("Hello my friends")
+        sched.wait(lambda: f1(x + 1, stop))
+
+def f2(x, stop):
+    if x < stop:
+        print("Printing from another thread kind offf!!!")
+        sched.wait_until(lambda: f2(x + 1, stop), 3)
+
+if __name__ == "__main__":
+    sched.wait(lambda: f1(0, 4))
+    sched.wait(lambda: f2(0, 3))
+    sched.run()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading 
+import time
 from time import sleep
 from collections import deque
 
@@ -6,14 +7,31 @@ from collections import deque
 class Scheduler:
     def __init__(self) -> None:
         self.call_queue = deque()
+        self.wait_list = []
 
     def wait(self,fn) -> None:
         self.call_queue.append(fn)
+    
+    def wait_until(self,fn,until) -> None:
+        now = time.time()
+        upto = now + until
+        self.wait_list.append((upto,fn))
+        self.wait_list = sorted(self.wait_list)
 
     def run(self) -> None:
-        while self.call_queue:
-            task = self.call_queue.popleft()
-            task()
+        while self.call_queue or self.wait_list:
+            
+            if not self.call_queue:
+                deadline, task = self.wait_list.pop(0)
+                delta = deadline - time.time()
+                
+                if delta > 0:
+                    sleep(delta)
+                    self.call_queue.append(task)
+
+            while self.call_queue:
+                task = self.call_queue.popleft()
+                task()
 
 
 sched = Scheduler()
@@ -21,14 +39,17 @@ sched = Scheduler()
 def f1(x,stop):
     if x < stop:
         print("Hello my friends")
-        sched.wait(lambda: f1(x+1,stop))
+        sched.wait(lambda: f1(x + 1,stop))
+
 
 def f2(x,stop):
     if x < stop:
         print("Printing from another thread kind offf!!!")
-        sched.wait(lambda: f2(x+1,stop))
+        sched.wait_until(lambda: f2(x + 1,stop),3)
+
 
 if __name__ == "__main__":
     sched.wait(lambda: f1(0,4))
     sched.wait(lambda: f2(0,3))
     sched.run()
+
